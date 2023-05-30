f2a26a1e08c92ed90f9418c673328d1006429483
Akash Jadhav <csegambitier@hotmail.com>
2023-05-30 15:35:19 +0530

    add RabbitMqFeature

---
+++ Elsa.RabbitMq/Extensions/ModuleExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Elsa.Features.Services;
+using Elsa.RabbitMq.Features;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+namespace Elsa.Extensions;
+
+public static class ModuleExtensions
+{
+    public static IModule UseRabbitMq(this IModule module, Action<RabbitMqFeature>? configure = default)
+    {
+        return module.Use(configure);
+    }
+}
---
+++ Elsa.RabbitMq/Features/RabbitMqFeature.cs
@@ -0,0 +1,17 @@
+﻿using Elsa.Features.Abstractions;
+using Elsa.Features.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elsa.RabbitMq.Features;
+
+public class RabbitMqFeature : FeatureBase
+{
+    public RabbitMqFeature(IModule module) : base(module)
+    {
+    }
+}
